c7addeb2fb2decf49bd4b9fe2025ee9aae4536cf
Papachristou, Ioanna <ioanna.papachristou@accnture.com>
2020-06-19 11:00:43 +0200

    Updated main to run/export the location table

--- my_package/__main__.py
+++ my_package/__main__.py
@@ -1,6 +1,7 @@
 # This file is executed when the module is run from the command line:
 # python -m my_package
 from my_package.example import greet
+from my_package.location_table import LocationTable
 
 
 if __name__ == '__main__':
@@ -10,3 +11,10 @@ if __name__ == '__main__':
     greet('developer')
 
     print("It worked!")
+
+    # 1. Creation of location table
+    loc_table = LocationTable()
+    location_table = loc_table.concat_sub_tables() # Create geodataframe
+    loc_table.export_to_parquet(location_table, 'output/location_table') # Export table to parquet format
+
+    # 2. Creation of shapefile table
