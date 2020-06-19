bc853678fb294abd55ff8cc01c774d177846d0b6
Papachristou, Ioanna <ioanna.papachristou@accnture.com>
2020-06-19 17:47:47 +0200

    Updated with functions for all spatial data

--- my_package/utils_shapefiles.py
+++ my_package/utils_shapefiles.py
@@ -8,6 +8,7 @@ Created on Thu Jun 19 11:14:40 2020
 """
 
 import geopandas as gpd
+import pandas as pd
 
 PATH = r'./data/'
 
@@ -16,8 +17,16 @@ class Shapefiles:
     Functions related to shapefiles.
     '''
     def __init__(self, hierarchy):
+        '''
+
+        :param hierarchy: 0 for the country level, 1-3 for the rest of regional levels
+        '''
         self.path = PATH
-        self.hierarchy = hierarchy
+
+        if hierarchy > 3 or hierarchy < 0:
+            raise ValueError("'hierarchy' should be an integer between 0 and 3, where 0 refers to the country level.")
+        else:
+            self.hierarchy = hierarchy
 
     def import_shapefile(self, country):
         '''
@@ -32,7 +41,6 @@ class Shapefiles:
     def shapefiles_list(self):
         '''
         Creates a list of shapefiles according to hierarchy.
-        :param hierarchy: 0 for the country level, 1-3 for the rest of regional levels
         :return: A list of geodataframes for all three countries (KEN, ETH, SOM) per hierarchy.
         '''
 
@@ -59,10 +67,31 @@ class Shapefiles:
             Ethiopia_3 = self.import_shapefile(country="_ETH_")
             gdf_list = [Kenya_3, Ethiopia_3]
 
-        else:
-            raise ValueError("'hierarchy' should be a number between 0 and 3, where 0 refers to the country level.")
-
         return gdf_list
+
+    def create_sub_tables(self, shp_list):
+        """
+        Creates sub-tables per hierarchy to be used for the final location table.
+        :param shp_list: A list of shapefiles
+        :return: A geodataframe of all location data for the selected hierarchy
+        """
+        # Concatenate shp
+        gdf = gpd.GeoDataFrame(pd.concat(shp_list, ignore_index=True, sort=False))
+        # Add new columns
+
+        gdf["locationID"] = gdf["GID_" + str(self.hierarchy)]
+        gdf["name"] = gdf["NAME_" + str(self.hierarchy)]
+        gdf["hierarchy"] = self.hierarchy
+        gdf["type"] = ""
+        if self.hierarchy == 0:
+            gdf["type"] = "Country"
+        elif self.hierarchy == 1:
+            gdf["type"] = "Region1"
+        elif self.hierarchy == 2:
+            gdf["type"] = "Region2"
+        elif self.hierarchy == 3:
+            gdf["type"] = "Region3"
+        return gdf
 
 if __name__ == '__main__':
 
@@ -73,3 +102,8 @@ if __name__ == '__main__':
     regions3_list = Shapefiles(3).shapefiles_list()
     #regions4 = Shapefiles(4).shapefiles_list() #only for testing, should raise value error
     #print(regions3)
+
+    print("------- Extracting geodataframes ---------")
+    countries = Shapefiles(0).create_sub_tables(countries_list)
+    print(countries.head())
+
