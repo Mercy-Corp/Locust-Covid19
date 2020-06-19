c731f0e6e57d4ea7b1f7e660ed29506980a8edaa
Papachristou, Ioanna <ioanna.papachristou@accnture.com>
2020-06-19 17:54:08 +0200

    Updated to extract both location and shapefile tables. Added automatic creation of data folders structure.

--- my_package/__main__.py
+++ my_package/__main__.py
@@ -2,7 +2,8 @@
 # python -m my_package
 from my_package.example import greet
 from my_package.location_table import LocationTable
-
+from my_package.shapefile_table import ShapefileTable
+import os
 
 if __name__ == '__main__':
     # A dummy main
@@ -12,9 +13,25 @@ if __name__ == '__main__':
 
     print("It worked!")
 
+    path = r'./data/'
+    # Create Input and Output folder structure
+    directories = [path + "data/input", path + "data/output"]
+    for directory in directories:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
     # 1. Creation of location table
+
+    print("------- Extracting location table ---------")
+
     loc_table = LocationTable()
-    location_table = loc_table.concat_sub_tables() # Create geodataframe
-    loc_table.export_to_parquet(location_table, 'output/location_table') # Export table to parquet format
+    location_table = loc_table.concat_sub_tables()      # Create geodataframe
+    loc_table.export_to_parquet(location_table, 'location_table')       # Export table to parquet format
 
     # 2. Creation of shapefile table
+
+    print("------- Extracting shapefile table ---------")
+
+    shp_table = ShapefileTable()
+    gdf_all = shp_table.concat_sub_tables()
+    shp_table.export_to_shp(gdf_all, 'shapefile_table')     # Export table to shp
