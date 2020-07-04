804d22f1e6d9ede8d273ea4133499685fd9283fb
Papachristou, Ioanna <ioanna.papachristou@accnture.com>
2020-07-04 12:01:27 +0200

    Created utils for flat files

---
+++ my_package/utils_flat_files.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+The aim of this module is to to list useful functions related to flat files.
+
+Created on Sat Jun 04 09:36:40 2020
+
+@author: ioanna.papachristou@accenture.com
+"""
+# Imports
+import time
+import pandas as pd
+
+#S3 paths
+# INPUT_PATH = r's3://mercy-locust-covid19-in-dev/inbound/sourcedata/Spatial/'
+# OUTPUT_PATH = r's3://mercy-locust-covid19-out-dev/location_dim/'
+
+#local paths
+INPUT_PATH = r'data/input/'
+OUTPUT_PATH = r'data/output/'
+
+class FlatFiles:
+    '''
+      Functions related to flat files.
+      '''
+
+    def __init__(self, path_in = INPUT_PATH, path_out = OUTPUT_PATH):
+        self.path_in = path_in
+        self.path_out = path_out
+        self.dates = pd.read_csv(self.path_out + 'date_23_06-2020.csv', sep=",")
+        self.dates['date'] = pd.to_datetime(self.dates['date'])
+
+    def add_date_id(self, df, column):
+        # Merge with dates and add dateID
+        df[column] = pd.to_datetime([f'{y}-01-01' for y in df[column]])
+        df = df.merge(self.dates, left_on=column, right_on='date', how='left')
+        return df
+
+    def select_columns_fact_table(self, df):
+        df_filtered = df[['factID', 'measureID', 'dateID', 'locationID', 'value']]
+        return df_filtered
+
+    def _date_today(self):
+        # Add today's date
+        todays_date = time.strftime("%Y-%m-%d")
+        return todays_date
+
+    def export_to_parquet(self, df, file_name):
+        '''
+        Exports a dataframe to a parquet format.
+        :param df: The dataframe to be exported
+        :param file_name: the name of the file to be exported
+        '''
+        df.to_parquet(self.path_out+file_name+".parquet", compression='uncompressed', index=False)
+        print("Dataframe exported to parquet format")
+
+    def export_to_csv(self, df, file_name):
+        '''
+        Exports a dataframe to a parquet format.
+        :param df: The dataframe to be exported
+        :param file_name: the name of the file to be exported
+        '''
+        # Export to csv
+        df.to_csv(self.path_out + file_name + '.csv', sep='|', encoding='utf-8', index=False)
+        print("Dataframe exported to csv format")
+
+    def export_output(self, df, file_name):
+        '''
+
+        :param df:
+        :param file_name:
+        :return:
+        '''
+        self.export_to_parquet(df, file_name)
+        self.export_to_csv(df, file_name)
+
+    def export_output_w_date(self, df, file_name):
+        date = self._date_today()
+        file_name = file_name + '_' + date
+
+        self.export_to_parquet(df, file_name)
+        self.export_to_csv(df, file_name)
