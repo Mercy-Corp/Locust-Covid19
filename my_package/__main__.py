784a923c1e3ef096d085451a4521bf294d8bfac1
Papachristou, Ioanna <ioanna.papachristou@accnture.com>
2020-07-15 12:13:48 +0200

    Updated main to include CroplandLocust class and calculation of crops affected by locust

--- my_package/__main__.py
+++ my_package/__main__.py
@@ -9,6 +9,8 @@ from measure_table import MeasuresTable
 from demand import DemandTable
 from cropland_area import Cropland
 from forageland_area import Forageland
+from forageland_locust import ForagelandLocust
+from cropland_locust import CroplandLocust
 #import os
 
 # S3 paths
@@ -88,4 +90,8 @@ if __name__ == '__main__':
 
     # 9. Calculation of forageland affected by locust
     print("------- Extracting forageland area affected by locust per district table ---------")
-    ForagelandLocust().export_table('Forage_impact_locust_district')
+    ForagelandLocust(INPUT_PATH, OUTPUT_PATH).export_table('Forage_impact_locust_district')
+
+    # 10 Calculation of cropland affected by locust
+    print("------- Extracting cropland area affected by locust per district table ---------")
+    CroplandLocust(INPUT_PATH, OUTPUT_PATH).export_table('Crops_impact_locust_district')
