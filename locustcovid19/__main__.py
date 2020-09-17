51dcae91816d132405d22e0d1c54c0faaddc0389
ioanna.papachristou <ioanna.papachristou@accenture.com>
2020-09-17 16:32:58 +0200

    Updated main to run the vegetation index

--- locustcovid19/__main__.py
+++ locustcovid19/__main__.py
@@ -156,6 +156,13 @@ if __name__ == '__main__':
         print("------- Extracting RVF risk table ---------")
         RiskTables(INPUT_PATH, OUTPUT_PATH).export_files('RVF')
 
+    elif module == 'vegetation':
+
+        print("------- Extracting vegetation index per district table ---------")
+        periods_list = [2021, 2022, 2023]
+        for period in periods_list:
+            VegetationTable(period, INPUT_PATH, OUTPUT_PATH).export_table('vegetation_table')
+
     else:
 
        print('Invalid module: {}'.format(module))
