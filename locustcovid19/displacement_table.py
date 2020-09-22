0bc63d8530119fc7ee497f79f8f29dda3e0b0825
ioanna.papachristou <ioanna.papachristou@accenture.com>
2020-09-22 18:34:11 +0200

    Documented displacement functions and modified filter_year function to accept new years.

--- locustcovid19/displacement_table.py
+++ locustcovid19/displacement_table.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-The aim of this module is to extract the displacement table.
+The aim of this module is to extract the displacements table.
 Data available from: https://data.worldbank.org/indicator/VC.IDP.NWDS
 
 Created on Thu Jun 24 09:36:40 2020
@@ -15,35 +15,44 @@ from utils.flat_files import FlatFiles
 
 COUNTRIES = ["Kenya", "Somalia", "Ethiopia", "Uganda", "South Sudan", "Sudan"]
 
-YEARS = [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
-
 class DisplacementTable:
     '''
-    This class creates the dislacements table.
+    This class creates the displacements table.
     '''
     def __init__(self, path_in, path_out):
         self.path_in = path_in
         self.path_out = path_out
-        self.displacement_df = pd.read_csv(self.path_in + "/social_cohesion/displacement/API_VC.IDP.NWDS_DS2_en_csv_v2_1223058.csv", skiprows=4, sep=",", encoding='utf-8')
+        self.displacement_df = pd.read_csv(self.path_in + "/social_cohesion/displacement/displacements.csv", skiprows=4, sep=",", encoding='utf-8')
         self.flats = FlatFiles(self.path_in, self.path_out)
 
     def filter_year(self, displacements_df, year):
-        # Create year column
-        displacements_df['year'] = str(year)
-        #Create value column
-        displacements_df['value'] = displacements_df[str(year)]
-        #Filter columns
-        displacements_year = displacements_df[['Country Code', 'year', 'value']]
+        '''
+
+        :param displacements_df: The displacements df.
+        :param year: The year in question.
+        :return: A df of displacements for the selected year.
+        '''
+        if str(year) in displacements_df.columns:
+            # Create year column
+            displacements_df['year'] = str(year)
+            #Create value column
+            displacements_df['value'] = displacements_df[str(year)]
+            #Filter columns
+            displacements_year = displacements_df[['Country Code', 'year', 'value']]
         return displacements_year
 
     def filter_data(self):
+        '''
+        Filters the dataset.
+        :return: A df with the displacement for the selected years and countries.
+        '''
         displacements = self.displacement_df
         # Filter countries
         displacements = displacements[displacements['Country Name'].isin(COUNTRIES)]
 
         #Filter years
         displacements_all_years = pd.DataFrame()
-        for year in YEARS:
+        for year in range(2000, 2030):
             displ_year = self.filter_year(displacements, year)
             displacements_all_years = displacements_all_years.append(displ_year)
 
@@ -52,7 +61,6 @@ class DisplacementTable:
     def add_ids_to_table(self):
         '''
         Merges with all other tables and extracts all ids.
-
         :return: The production dataframe with all columns as defined in the data model.
         '''
         displacements = self.filter_data().reset_index(drop=True)
@@ -64,7 +72,7 @@ class DisplacementTable:
         displacements['factID'] = 'DISP_' + displacements.index.astype(str)
 
         # Add dateID
-        displacements['dateID'] = displacements['year'].astype(str)+ '0101'
+        displacements['dateID'] = displacements['year'].astype(str) + '0101'
         displacements['dateID'] = displacements['dateID'].astype(int)
         #displacements = self.flats.add_date_id(displacements, 'year')
 
@@ -81,7 +89,6 @@ class DisplacementTable:
         '''
         displacement_df = self.add_ids_to_table()
         #self.flats.export_csv_w_date(displacement_df, 'displacement_table')
-        #self.flats.export_parquet_w_date(displacement_df, 'displacement_table')
         self.flats.export_to_parquet(displacement_df, '/displacement_fact/displacement_table')
 
 if __name__ == '__main__':
