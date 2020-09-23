42cb51c5e39a239ce24a4be8817e632bcd152eb8
ioanna.papachristou <ioanna.papachristou@accenture.com>
2020-09-23 13:30:56 +0200

    DOcumented functions in refugees_table.py

--- locustcovid19/refugees_table.py
+++ locustcovid19/refugees_table.py
@@ -13,12 +13,11 @@ import yaml
 import pandas as pd
 from utils.flat_files import FlatFiles
 
-
 COUNTRIES = ["Kenya", "Somalia", "Ethiopia", "Uganda", "South Sudan", "Sudan"]
 
 class RefugeesTable:
     '''
-    This class creates the dislacements table.
+    This class creates the refugees table.
     '''
     def __init__(self, path_in, path_out):
         self.path_in = path_in
@@ -27,6 +26,11 @@ class RefugeesTable:
         self.flats = FlatFiles(self.path_in, self.path_out)
 
     def convert_to_numeric(self, df):
+        '''
+        Converts columns to numeric.
+        :param df: The data frame.
+        :return: A df with the selected columns transformed to numeric.
+        '''
         columns = ['Refugees under UNHCR’s mandate', 'Asylum-seekers',
        'IDPs of concern to UNHCR', 'Venezuelans displaced abroad',
        'Stateless persons', 'Others of concern']
@@ -37,7 +41,12 @@ class RefugeesTable:
         return df
 
     def add_ids(self):
+        '''
+        Adds the fact tables ids.
+        :return:  A filtered dataframe with the columns we need for fact tables.
+        '''
         refugees_df = self.convert_to_numeric(self.refugees_df)
+
         # Calculate value as the sum of all refugees (excluded 'Venezuelans displaced abroad' - all nans)
         refugees_df['value'] = refugees_df['Refugees under UNHCR’s mandate'] + refugees_df['Asylum-seekers'] + refugees_df['IDPs of concern to UNHCR'] + refugees_df['Stateless persons'] + refugees_df['Others of concern']
 
@@ -64,8 +73,7 @@ class RefugeesTable:
         Exports to parquet format.
         '''
         refugees_df = self.add_ids()
-        #self.flats.export_csv_w_date(refugees_df, 'refugees_table')
-        #self.flats.export_parquet_w_date(refugees_df, 'refugees_table')
+        #self.flats.export_csv_w_date(refugees_df, '/refugees_table')
         self.flats.export_to_parquet(refugees_df, '/refugees_fact/refugees_table')
 
 if __name__ == '__main__':
